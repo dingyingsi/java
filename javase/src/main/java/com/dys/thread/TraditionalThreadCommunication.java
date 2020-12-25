6411575fe5f02fa7988894d3ac2a70cba970b413
dingyingsi <dingyingsi@126.com>
2020-12-25 10:32:28 +0800

    a

--- javase/src/main/java/com/dys/thread/TraditionalThreadCommunication.java
+++ javase/src/main/java/com/dys/thread/TraditionalThreadCommunication.java
@@ -8,57 +8,51 @@ package com.dys.thread;
  * sleep()睡眠后不出让系统资源，wait 让其他线程可以占用 CPU；
  * sleep(milliseconds)需要指定一个睡眠时间，时间一到会自动唤醒。而 wait()需要配合 notify()
  * 或者 notifyAll()使用。
- *
- *
  */
 public class TraditionalThreadCommunication {
 
-  public static void main(String[] args) {
-     final Business business = new Business();
-     new Thread(
-          new Runnable() {
-            @Override
-            public void run() {
-              for(int i=1;i<=50;i++){
-                 business.sub(i);
-              }
-            }
-          }
-     ).start();
-     for(int i=1;i<=50;i++){
-       business.main(i);
-     }
-  }
-}
-class Business {
-  private boolean bShouldSub = true;
-  public synchronized void sub(int i){
-    while(!bShouldSub){
-       try {
-       this.wait();
-     } catch (InterruptedException e) {
-       e.printStackTrace();
-     }
+    public static void main(String[] args) throws Exception {
+        final Business business = new Business();
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = 1; i <= 50; i++) {
+                            try {
+                                business.sub(i);
+                            } catch (Exception e) {}
+                        }
+                    }
+                }
+        ).start();
+        for (int i = 1; i <= 50; i++) {
+            business.main(i);
+        }
     }
-     for(int j=1;j<=10;j++){
-       System.out.println("sub thread sequence of " + j + ",loop of " + i);
-     }
-    bShouldSub = false;
-    this.notify();
-  }
-  
-  public synchronized void main(int i){
-     while(bShouldSub){
-       try {
-          this.wait();
-       } catch (InterruptedException e) {
-          e.printStackTrace();
-       }
-     }
-     for(int j=1;j<=100;j++){
-       System.out.println("main thread sequence of " + j + ",loop of " + i);
-     }
-     bShouldSub = true;
-     this.notify();
-  }
+}
+
+class Business {
+    private boolean bShouldSub = true;
+
+    public synchronized void sub(int i) throws Exception {
+        while (!bShouldSub) {
+            this.wait();
+        }
+        for (int j = 1; j <= 10; j++) {
+            System.out.println("sub thread sequence of " + j + ",loop of " + i);
+        }
+        bShouldSub = false;
+        this.notify();
+    }
+
+    public synchronized void main(int i) throws Exception {
+        while (bShouldSub) {
+            this.wait(); // 此处阻塞调用这个方法的线程，把线程放到这个对象对应的阻塞队列中
+        }
+        for (int j = 1; j <= 100; j++) {
+            System.out.println("main thread sequence of " + j + ",loop of " + i);
+        }
+        bShouldSub = true;
+        this.notify();
+    }
 }
--- javase/src/main/java/com/dys/thread/TraditionalThreadSynchronized.java
+++ javase/src/main/java/com/dys/thread/TraditionalThreadSynchronized.java
@@ -1,61 +1,64 @@
 package com.dys.thread;
 
 public class TraditionalThreadSynchronized {
-  public static void main(String[] args) {
-     new TraditionalThreadSynchronized().init();
-  }
-  private void init(){
-     final Outputer outputer = new Outputer();
-     new Thread(new Runnable(){
-       @Override
-       public void run() {
-          while(true){
-            try {
-              Thread.sleep(10);
-            } catch (InterruptedException e) {
-              // TODO Auto-generated catch block
-              e.printStackTrace();
+    public static void main(String[] args) {
+        new TraditionalThreadSynchronized().init();
+    }
+
+    private void init() {
+        final Outputer outputer = new Outputer();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    outputer.output("zhangxiaoxiang");
+                }
             }
-            outputer.output("zhangxiaoxiang");
-          }
-       }
-     }).start();
-     new Thread(new Runnable(){
-       @Override
-       public void run() {
-          while(true){
-            try {
-              Thread.sleep(10);
-            } catch (InterruptedException e) {
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                    }
+                    outputer.output3("lihuoming");
+                }
             }
-            outputer.output3("lihuoming");
-          }
-       }
-     }).start();
-  }
-  static class Outputer{
-     public void output(String name){
-       int len = name.length();
-       synchronized (Outputer.class) {
-          for(int i=0;i<len;i++){
-            System.out.print(name.charAt(i));
-          }
-          System.out.println();
-       }
-     }
-     public synchronized void output2(String name){
-       int len = name.length();
-       for(int i=0;i<len;i++){
-          System.out.print(name.charAt(i));
-       }
-       System.out.println();
-     }
-     public static synchronized void output3(String name){
-       int len = name.length();
-       for(int i=0;i<len;i++){
-          System.out.print(name.charAt(i));
-       }
-       System.out.println();
-     } 
-  }
+        }).start();
+    }
+
+    static class Outputer {
+        public void output(String name) {
+            int len = name.length();
+            synchronized (Outputer.class) {
+                for (int i = 0; i < len; i++) {
+                    System.out.print(name.charAt(i));
+                }
+                System.out.println();
+            }
+        }
+
+        public synchronized void output2(String name) {
+            int len = name.length();
+            for (int i = 0; i < len; i++) {
+                System.out.print(name.charAt(i));
+            }
+            System.out.println();
+        }
+
+        public static synchronized void output3(String name) {
+            int len = name.length();
+            for (int i = 0; i < len; i++) {
+                System.out.print(name.charAt(i));
+            }
+            System.out.println();
+        }
+    }
 }
